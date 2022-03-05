f2ad585f300af57a6741592d85c755951277cd77
jmservera <8036360+jmservera@users.noreply.github.com>
2022-03-06 00:08:42 +0100

    build not needed, use ncc directly

--- dist/knowledgebase.d.ts
+++
@@ -1,7 +0,0 @@
-import * as qnamaker from "@azure/cognitiveservices-qnamaker";
-declare function update(
-  api_key: string,
-  endpoint: string,
-  id: string
-): Promise<qnamaker.QnAMakerModels.KnowledgebaseUpdateResponse>;
-export { update };
